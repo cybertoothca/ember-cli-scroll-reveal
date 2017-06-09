59656661d35c007a3c199c654c97a060a93ab7a9
Dan Nelson <dnelson@cybertooth.ca>
2017-06-08 22:26:40 -0700

    Fixing blueprint.

--- blueprints/ember-cli-scroll-reveal/index.js
+++ blueprints/ember-cli-scroll-reveal/index.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  description: 'Ember-ized scroll animations for web and mobile browsers.',
-  name: 'ember-cli-scroll-reveal',
   afterInstall: function (/*options*/) {
     this.addBowerPackagesToProject([
       {name: 'scrollreveal'}
     ]);
+  },
+  description: 'Ember-ized scroll animations for web and mobile browsers.',
+  name: 'ember-cli-scroll-reveal',
+  normalizeEntityName: function (entityName) {
+    // Normalize and validate entity name here.
+    return entityName;
   }
 };
