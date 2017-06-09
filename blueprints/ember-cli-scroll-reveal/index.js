fde0b4918620401ee12700c3daa5cdf717a61e69
Dan Nelson <dnelson@cybertooth.ca>
2017-06-08 22:44:03 -0700

    More alpha testing.

--- blueprints/ember-cli-scroll-reveal/index.js
+++ blueprints/ember-cli-scroll-reveal/index.js
@@ -1,9 +1,7 @@
 /* eslint-env node */
 module.exports = {
   afterInstall: function (/*options*/) {
-    this.addBowerPackagesToProject([
-      {name: 'scrollreveal'}
-    ]);
+    return this.addBowerPackageToProject('scrollreveal');
   },
   description: 'Ember-ized scroll animations for web and mobile browsers.',
   name: 'ember-cli-scroll-reveal',
